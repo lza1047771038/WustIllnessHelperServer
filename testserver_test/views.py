146fa4e5037b2248664f1bfc889a0f70f60cc211
TenderLove <1047771038@qq.com>
2019-10-31 16:13:32 +0800

    修改图片数据库结构

--- testserver_test/views.py
+++ testserver_test/views.py
@@ -407,7 +407,7 @@ def uploadImage(request):
         # FileLogger.log_info("upload_image", uploadImg, FileLogger.IMAGE_HANDLER)
 
         # 返回图片的url以供访问
-        return JsonResponse({"url": uploadImg.getImageUrl() + "   " + uploadImg.getImagePath()})
+        return JsonResponse({"url": uploadImg.getImageUrl()})
     return render(request, 'test.html')
 
 
