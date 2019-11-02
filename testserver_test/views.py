b5811ac89a472e7de2d4bd1aafee47304fc9e0f5
TenderLove <1047771038@qq.com>
2019-11-02 22:33:52 +0800

    增加推文信息的下载与上传

--- testserver_test/views.py
+++ testserver_test/views.py
@@ -236,8 +236,11 @@ def NotificationList(request):
             books = paginator.page(paginator.num_pages)
         for item in books:
             image = UploadImage.objects.all().filter(themeid=item['themeid']).order_by('id').first()
+            user = UserInfo.objects.all().filter(userId=item['author_id_id']).order_by('id').first()
             if image is not None:
                 item['image'] = image.getImageUrl()
+            if user is not None:
+                item['username'] = user.username
         data['data'] = list(books)
         return JsonResponse(data)
 
