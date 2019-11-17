f619ad9a9e6c826765ddb2d2f2a23d37f372b335
TenderLove <1047771038@qq.com>
2019-11-17 16:34:43 +0800

    增加推文信息的下载与上传

--- testserver_test/views.py
+++ testserver_test/views.py
@@ -69,8 +69,8 @@ def getUserInfo(request):
 
 
 def login(request):  # 判断请求方法
-    if request.method == 'GET':
-        parm = request.GET
+    if request.method == 'POST':
+        parm = request.POST
         new_userinfo = UserInfo()
         new_userinfo.userId = parm.get("userid", "null")
         new_userinfo.password = parm.get("password", 'null')
